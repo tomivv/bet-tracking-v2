35f7c75f3001b6239de614136e336f60ee48d4cb
Tomi Niiranen <tominiiranen@hotmail.com>
2022-02-18 12:48:34 +0000

    Changed how dashboard looks; Added layout with navbar;

--- bet-tracking-client/components/BetDisplay.tsx
+++ bet-tracking-client/components/BetDisplay.tsx
@@ -12,24 +12,41 @@ interface Props {
 
 const BetDisplay = ({ title, data }: Props) => {
   return (
-    <div className={styles.period}>
-      <h1>{title}</h1>
-      <div className={styles.row}>
-        <div className={styles.item}>
-          <h2>kerroin</h2>
-          <p>{(data.odds / data.amount).toFixed(2)}</p>
-        </div>
-        <div className={styles.item}>
-          <h2>Tulos</h2>
-          <p>{(data.result / data.amount).toFixed(2)}</p>
-        </div>
-        <div className={styles.item}>
-          <h2>Määrä</h2>
-          <p>{data.amount}</p>
-        </div>
+    <div className={styles.card}>
+      <h2>{title}</h2>
+      <div className={styles.card_body}>
+        <p>Kerroin</p>
+        <p>{(data.odds / data.amount).toFixed(2)}</p>
+      </div>
+      <div className={styles.card_body}>
+        <p>Tulos</p>
+        <p>{(data.result / data.amount).toFixed(2)}</p>
+      </div>
+      <div className={styles.card_body}>
+        <p>Määrä</p>
+        <p>{data.amount}</p>
       </div>
     </div>
-  );
+  )
+  // return (
+  //   <div className={styles.period}>
+  //     <h1>{title}</h1>
+  //     <div className={styles.row}>
+  //       <div className={styles.item}>
+  //         <h2>kerroin</h2>
+  //         <p>{(data.odds / data.amount).toFixed(2)}</p>
+  //       </div>
+  //       <div className={styles.item}>
+  //         <h2>Tulos</h2>
+  //         <p>{(data.result / data.amount).toFixed(2)}</p>
+  //       </div>
+  //       <div className={styles.item}>
+  //         <h2>Määrä</h2>
+  //         <p>{data.amount}</p>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 }
 
 export default BetDisplay;
